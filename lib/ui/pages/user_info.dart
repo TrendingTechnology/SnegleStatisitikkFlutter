6b27caf460554d55b7a00e28f541d087eb2441ee
ProgrammerChris <chris.tvedt@gmail.com>
2019-12-11 22:06:18 +0100

    asd

--- lib/ui/pages/user_info.dart
+++ lib/ui/pages/user_info.dart
@@ -1,17 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:slugflutter/ui/themes/theme.dart';
 
-class UserInfo extends StatelessWidget {
-  UserInfo({Key key}) : super(key: key);
+class UserInfo extends StatefulWidget  {
+  @override
+  _UserInfoState createState() => _UserInfoState();
+
+}
+
+class _UserInfoState extends State<UserInfo> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomTheme.getTheme.backgroundColor,
-      body: Center(
-        child:  Text("User Info")
-      )
+      body: Builder(
+        builder: (context) => AlertDialog(
+          backgroundColor: CustomTheme.getTheme.dialogBackgroundColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text('Fangst',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0
+              ),
+              textAlign: TextAlign.center,
+            ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Divider(
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 20.0),
+                  child: Text('Antall',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  //Dropdown for fylke her!
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child:Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RawMaterialButton(
+                        onPressed: () { 
+                          // SEND _formController.text to DB together with al the other date the model needs.
+                          FocusScope.of(context).requestFocus(FocusNode());
+                          Navigator.pop(context);
+                        },
+                        child: Container( 
+                          child: Image(
+                            image: AssetImage('assets/buttons/check_button.png'),
+                            width: 65.0,
+                            height: 65.0,
+                          ),
+                        ),
+                        shape: CircleBorder(),
+                      ),
+                      RawMaterialButton(
+                        onPressed: () {
+                          FocusScope.of(context).requestFocus(FocusNode());
+                          Navigator.pop(context);
+                        },
+                        child: Container( 
+                          child: Image(
+                            image: AssetImage('assets/buttons/white_cancel_button.png'),
+                            width: 65.0,
+                            height: 65.0,
+                          ),
+                        ),
+                        shape: CircleBorder(),
+                      ),
+                    ],
+                  ),
+                ),
+              ], 
+            ),
+          ),
+        ),
+      ),
     );
   }
-
 }
