a7a90cd486ae5083428a4873cff70ac962587f29
ProgrammerChris <chris.tvedt@gmail.com>
2020-01-10 23:41:32 +0100

    Added drag to refresh on main page

--- lib/ui/pages/main.dart
+++ lib/ui/pages/main.dart
@@ -1,4 +1,6 @@
 // To be retrived from DB
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:slugflutter/database/controllers/APIcontroller.dart';
@@ -67,216 +69,227 @@ class _MainPageState extends State<MainPage>  {
     });
   }
 
-  ListView _pageMainWidget() {
-    return ListView(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 30.0),
-          child: Builder(
-            builder: (context) => RawMaterialButton(
-              onPressed: () =>
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(
-                    builder: (context) => AppInfo()
+  Future<void> _refresh() async {
+    setState(() {
+      
+    });
+  }
+
+
+  RefreshIndicator _pageMainWidget() {
+    return RefreshIndicator(
+      onRefresh: _refresh,
+      child:  ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 30.0),
+            child: Builder(
+              builder: (context) => RawMaterialButton(
+                onPressed: () =>
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context) => AppInfo()
+                    ),
+                  ),
+                  child: Image(
+                    image: AssetImage('assets/images/dying_slug.png'),
+                    width: 257.0,
+                    height: 252.0,
+                  ),
+                shape: CircleBorder(),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 5.0, left: 30.0),
+                child: Text('Mine funn',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: CustomTheme.getTheme.textSelectionColor
                   ),
                 ),
-                child: Image(
-                  image: AssetImage('assets/images/dying_slug.png'),
-                  width: 257.0,
-                  height: 252.0,
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 5.0, right: 30),
+                alignment: Alignment.centerRight,
+                child: Text('${User.toMap()['totalFinds']}',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(                        
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: CustomTheme.getTheme.textSelectionColor
+                  ),
                 ),
-              shape: CircleBorder(),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30, right: 30),
+            child: Divider(
+              color: CustomTheme.getTheme.textSelectionColor,
             ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(top: 5.0, left: 30.0),
-              child: Text('Mine funn',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: CustomTheme.getTheme.textSelectionColor
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 5.0, left: 30.0),
+                child: Text('Min kommune',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: CustomTheme.getTheme.textSelectionColor,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 5.0, right: 30),
-              alignment: Alignment.centerRight,
-              child: Text('${User.toMap()['totalFinds']}',
-                textAlign: TextAlign.start,
-                style: TextStyle(                        
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: CustomTheme.getTheme.textSelectionColor
-                ),
+              Container(
+                padding: const EdgeInsets.only(top: 5.0, right: 30.0),
+                child: _getKommuneFindings(User.toMap()['kommune']),
               ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 10.0, left: 30, right: 30),
-          child: Divider(
-            color: CustomTheme.getTheme.textSelectionColor,
+            ],
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(top: 5.0, left: 30.0),
-              child: Text('Min kommune',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: CustomTheme.getTheme.textSelectionColor,
-                ),
-              ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30, right: 30),
+            child: Divider(
+              color: CustomTheme.getTheme.textSelectionColor,
             ),
-            Container(
-              padding: const EdgeInsets.only(top: 5.0, right: 30.0),
-              child: _getKommuneFindings(User.toMap()['kommune']),
-            ),
-          ],
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 10.0, left: 30, right: 30),
-          child: Divider(
-            color: CustomTheme.getTheme.textSelectionColor,
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(top: 5.0, left: 30.0),
-              child: Text('Mitt fylke',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: CustomTheme.getTheme.textSelectionColor,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 5.0, left: 30.0),
+                child: Text('Mitt fylke',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: CustomTheme.getTheme.textSelectionColor,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 5.0, right: 30.0),
-              child: _getFylkeFindings(User.toMap()['fylke'])
-            ),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 45.0),
-              child: Builder(
-                builder: (context) => RawMaterialButton(
-                  onPressed: () => StatsDialog.showStatsDialog(context),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black54,
-                          blurRadius: 20.0, // has the effect of softening the shadow
-                          spreadRadius: 5.0, // has the effect of extending the shadow
-                          offset: Offset(
-                            10.0, // horizontal, move right 10
-                            10.0, // vertical, move down 10
+              Container(
+                padding: const EdgeInsets.only(top: 5.0, right: 30.0),
+                child: _getFylkeFindings(User.toMap()['fylke'])
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 45.0),
+                child: Builder(
+                  builder: (context) => RawMaterialButton(
+                    onPressed: () => StatsDialog.showStatsDialog(context),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 20.0, // has the effect of softening the shadow
+                            spreadRadius: 5.0, // has the effect of extending the shadow
+                            offset: Offset(
+                              10.0, // horizontal, move right 10
+                              10.0, // vertical, move down 10
+                            ),
                           ),
-                        ),
-                      ],
-                      shape: BoxShape.circle
-                    ),
-                    child: Image(
-                      image: AssetImage('assets/buttons/stats_button.png'),
-                      width: 120.0,
-                      height: 120.0,
-                    ),
-                  ),
-                  shape: CircleBorder(),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 80, left: 50.0),
-              child: Builder(
-                builder: (context) => RawMaterialButton(
-                  onPressed: () async {
-                    var _submitted = await AddFindDialog.showAddFindDialog(context);
-                    if (_submitted == true)  {
-                      setState(() {
-                        
-                      });
-                    }
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black54,
-                          blurRadius: 20.0, // has the effect of softening the shadow
-                          spreadRadius: 5.0, // has the effect of extending the shadow
-                          offset: Offset(
-                            10.0, // horizontal, move right 10
-                            10.0, // vertical, move down 10
-                          ),
-                        ),
-                      ],
-                      shape: BoxShape.circle
-                    ),   
-                    child: Image(
-                      image: AssetImage('assets/buttons/add_button.png'),
-                      width: 100.0,
-                      height: 100.0,
-                    ),
-                  ),
-                  shape: CircleBorder(),
-                ),
-              ),
-            ),
-          ],
-        ),
-        Padding(
-          padding: EdgeInsets.only(right: 40.0),
-          child: Builder(
-            builder: (context) => RawMaterialButton(
-              onPressed: () async {
-                var _submitted = await UserInfoDialog.showUserInfoDialog(context);
-                if (_submitted == true)  {
-                  setState(() {
-                    
-                  });
-                }
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black54,
-                      blurRadius: 20.0, // has the effect of softening the shadow
-                      spreadRadius: 5.0, // has the effect of extending the shadow
-                      offset: Offset(
-                        10.0, // horizontal, move right 10
-                        10.0, // vertical, move down 10
+                        ],
+                        shape: BoxShape.circle
+                      ),
+                      child: Image(
+                        image: AssetImage('assets/buttons/stats_button.png'),
+                        width: 120.0,
+                        height: 120.0,
                       ),
                     ),
-                  ],
-                  shape: BoxShape.circle
-                ),  
-                child: Image(
-                  image: AssetImage('assets/buttons/user_button.png'),
-                  width: 80.0,
-                  height: 80.0,
+                    shape: CircleBorder(),
+                  ),
                 ),
               ),
-              shape: CircleBorder(),
+              Padding(
+                padding: EdgeInsets.only(top: 80, left: 50.0),
+                child: Builder(
+                  builder: (context) => RawMaterialButton(
+                    onPressed: () async {
+                      var _submitted = await AddFindDialog.showAddFindDialog(context);
+                      if (_submitted == true)  {
+                        setState(() {
+                          
+                        });
+                      }
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 20.0, // has the effect of softening the shadow
+                            spreadRadius: 5.0, // has the effect of extending the shadow
+                            offset: Offset(
+                              10.0, // horizontal, move right 10
+                              10.0, // vertical, move down 10
+                            ),
+                          ),
+                        ],
+                        shape: BoxShape.circle
+                      ),   
+                      child: Image(
+                        image: AssetImage('assets/buttons/add_button.png'),
+                        width: 100.0,
+                        height: 100.0,
+                      ),
+                    ),
+                    shape: CircleBorder(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 40.0),
+            child: Builder(
+              builder: (context) => RawMaterialButton(
+                onPressed: () async {
+                  var _submitted = await UserInfoDialog.showUserInfoDialog(context);
+                  if (_submitted == true)  {
+                    setState(() {
+                      
+                    });
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 20.0, // has the effect of softening the shadow
+                        spreadRadius: 5.0, // has the effect of extending the shadow
+                        offset: Offset(
+                          10.0, // horizontal, move right 10
+                          10.0, // vertical, move down 10
+                        ),
+                      ),
+                    ],
+                    shape: BoxShape.circle
+                  ),  
+                  child: Image(
+                    image: AssetImage('assets/buttons/user_button.png'),
+                    width: 80.0,
+                    height: 80.0,
+                  ),
+                ),
+                shape: CircleBorder(),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
+    
   }
 
   _getKommuneFindings(String kommune) {
